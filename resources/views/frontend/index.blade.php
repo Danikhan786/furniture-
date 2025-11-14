adcf3e44bdf38daccb2a6fa46b704d625d9a872e
Ali Hassan <tech.danishkhan@gmail.com>
2025-11-14 20:34:52 +0500

    Update responsive styles and adjust homepage sections
    Refined responsive CSS for better mobile layout, including adjustments to hero, footer, and 'why choose' sections. Moved the product section below 'why choose us' in the homepage Blade template and commented out the blog section.

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -22,66 +22,6 @@
     </div>
 <!-- End Hero Section -->
 
-<!-- Start Product Section -->
-<div class="product-section">
-    <div class="container">
-        <div class="row">
-
-            <!-- Start Column 1 -->
-            <div class="col-md-12 col-lg-3 mb-5 mb-lg-0">
-                <h2 class="mb-4 section-title">Crafted with excellent material.</h2>
-                <p class="mb-4">Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique. </p>
-                <p><a href="shop.html" class="btn">Explore</a></p>
-            </div> 
-            <!-- End Column 1 -->
-
-            <!-- Start Column 2 -->
-            <div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
-                <a class="product-item" href="cart.html">
-                    <img src="frontend/images/product-1.png" class="img-fluid product-thumbnail">
-                    <h3 class="product-title">Nordic Chair</h3>
-                    <strong class="product-price">$50.00</strong>
-
-                    <span class="icon-cross">
-                        <img src="frontend/images/cross.svg" class="img-fluid">
-                    </span>
-                </a>
-            </div> 
-            <!-- End Column 2 -->
-
-            <!-- Start Column 3 -->
-            <div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
-                <a class="product-item" href="cart.html">
-                    <img src="frontend/images/product-2.png" class="img-fluid product-thumbnail">
-                    <h3 class="product-title">Kruzo Aero Chair</h3>
-                    <strong class="product-price">$78.00</strong>
-
-                    <span class="icon-cross">
-                        <img src="frontend/images/cross.svg" class="img-fluid">
-                    </span>
-                </a>
-            </div>
-            <!-- End Column 3 -->
-
-            <!-- Start Column 4 -->
-            <div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
-                <a class="product-item" href="cart.html">
-                    <img src="frontend/images/product-3.png" class="img-fluid product-thumbnail">
-                    <h3 class="product-title">Ergonomic Chair</h3>
-                    <strong class="product-price">$43.00</strong>
-
-                    <span class="icon-cross">
-                        <img src="frontend/images/cross.svg" class="img-fluid">
-                    </span>
-                </a>
-            </div>
-            <!-- End Column 4 -->
-
-        </div>
-    </div>
-</div>
-<!-- End Product Section -->
-
 <!-- Start Why Choose Us Section -->
 <div class="why-choose-section">
     <div class="container">
@@ -144,6 +84,66 @@
     </div>
 </div>
 <!-- End Why Choose Us Section -->
+
+<!-- Start Product Section -->
+<div class="product-section">
+    <div class="container">
+        <div class="row">
+
+            <!-- Start Column 1 -->
+            <div class="col-md-12 col-lg-3 mb-5 mb-lg-0">
+                <h2 class="mb-4 section-title">Crafted with excellent material.</h2>
+                <p class="mb-4">Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique. </p>
+                <p><a href="shop.html" class="btn">Explore</a></p>
+            </div> 
+            <!-- End Column 1 -->
+
+            <!-- Start Column 2 -->
+            <div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
+                <a class="product-item" href="cart.html">
+                    <img src="frontend/images/product-1.png" class="img-fluid product-thumbnail">
+                    <h3 class="product-title">Nordic Chair</h3>
+                    <strong class="product-price">$50.00</strong>
+
+                    <span class="icon-cross">
+                        <img src="frontend/images/cross.svg" class="img-fluid">
+                    </span>
+                </a>
+            </div> 
+            <!-- End Column 2 -->
+
+            <!-- Start Column 3 -->
+            <div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
+                <a class="product-item" href="cart.html">
+                    <img src="frontend/images/product-2.png" class="img-fluid product-thumbnail">
+                    <h3 class="product-title">Kruzo Aero Chair</h3>
+                    <strong class="product-price">$78.00</strong>
+
+                    <span class="icon-cross">
+                        <img src="frontend/images/cross.svg" class="img-fluid">
+                    </span>
+                </a>
+            </div>
+            <!-- End Column 3 -->
+
+            <!-- Start Column 4 -->
+            <div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
+                <a class="product-item" href="cart.html">
+                    <img src="frontend/images/product-3.png" class="img-fluid product-thumbnail">
+                    <h3 class="product-title">Ergonomic Chair</h3>
+                    <strong class="product-price">$43.00</strong>
+
+                    <span class="icon-cross">
+                        <img src="frontend/images/cross.svg" class="img-fluid">
+                    </span>
+                </a>
+            </div>
+            <!-- End Column 4 -->
+
+        </div>
+    </div>
+</div>
+<!-- End Product Section -->
 
 <!-- Start We Help Section -->
 <div class="we-help-section">
@@ -321,7 +321,7 @@
 <!-- End Testimonial Slider -->
 
 <!-- Start Blog Section -->
-<div class="blog-section">
+{{-- <div class="blog-section">
     <div class="container">
         <div class="row mb-5">
             <div class="col-md-6">
@@ -372,6 +372,6 @@
 
         </div>
     </div>
-</div>
+</div> --}}
 <!-- End Blog Section -->	
 @endsection
